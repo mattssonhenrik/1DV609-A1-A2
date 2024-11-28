1ee1da6f0269c7475fcf27ddf00cb000d5acdeab
Henrik Mattsson <hm222yj@student.lnu.se>
2024-11-28 10:21:11 +0100

    Add basic folder structure
    Added a MVC folder structure and made sure the project can build and run.

---
+++ app/src/main/java/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Main {
+    public Main() {
+        System.out.println(helloWorld());    
+    }
+    
+
+
+public String helloWorld (){
+    return "Hello World";
+}
+
+public static void main(String[] args) {
+    new Main();
+}
+
+}
